d80aa3caa88cda90ec10964aaeb3c5f60f53c4c1
Super Manito <68613938+SuperManito@users.noreply.github.com>
2022-01-05 09:01:27 +0800

    Update

--- extra.sh
+++ extra.sh
@@ -247,7 +247,7 @@ for author in $author_list; do
         ;;
       *)
         if [[ -z ${script_cron_standard} ]]; then
-          tmp1=$(grep -E "cron|script-path|tag|\* \*|$name" $ScriptsDir/$name | grep -Ev "^http.*:" | head -1 | perl -pe '{s|[a-zA-Z\"\.\=\:\:\_]||g;}')
+          tmp1=$(grep -E "cron|script-path|tag|\* \*|$name" $ScriptsDir/$name | grep -Ev "^http.*:" | head -1 | perl -pe '{s|[a-zA-Z\"\.\=\:\_]||g;}')
           ## 判断开头
           tmp2=$(echo "${tmp1}" | awk -F '[0-9]' '{print$1}' | sed 's/\*/\\*/g; s/\./\\./g')
           ## 判断表达式的第一个数字（分钟）
